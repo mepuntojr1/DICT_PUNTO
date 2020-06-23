aafefd0d618d731e0d5cfa25ba2693f4957bc5e7
Eduardo Punto Jr <mepuntojr.1@tip.edu.ph>
2020-06-23 17:32:47 +0800

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'DICT';
+  name = 'Department of Information and Communications Technology';
+}
